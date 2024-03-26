be5e7c858abe28f1c610e667bb06ef132d28e201
Nolan Jannotta <anjannotta@verizon.net>
2024-03-26 10:28:46 -0700

    3/25/26

--- app/contracts/artgenerator/page.jsx
+++ app/contracts/artgenerator/page.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { useQuery } from '@tanstack/react-query'
-import {contract} from "../../constants"
+import {contract} from "../../contract"
 
 
 async function getArtGenerators() {
