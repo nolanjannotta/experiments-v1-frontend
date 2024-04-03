5f9077390545bbad54fe5558e583b6c2aec85287
Nolan Jannotta <anjannotta@verizon.net>
2024-04-03 09:51:35 -0700

    testing

--- app/constants.js
+++ app/constants.js
@@ -3,4 +3,4 @@ export const artAddress = "0xa088b582D05346Dc57dc9eBc8217DA30b24eB388";
 export const ZERO_ADDRESS= "0x0000000000000000000000000000000000000000"
 
 
-export const FRAME_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000/' : 'https://experiments-v1.vercel.app/';
+export const FRAME_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://experiments-v1.vercel.app';
