cc45d2aaaa0a91cb11dfe01756a2e5dd529659a7
Nolan Jannotta <anjannotta@verizon.net>
2024-05-13 21:27:28 -0700

    5/13

---
+++ app/frames/images/token/route.js
@@ -0,0 +1,38 @@
+import { ImageResponse } from "next/og";
+import Card from "@/components/Card";
+import {getUri} from "@/app/frameConfig";
+import { signerContract } from "@/app/frameConfig";
+
+
+
+export async function GET(request) {
+    const tokenId = request.nextUrl.searchParams.get("tokenId")
+
+    let uri;
+    uri = await getUri(tokenId)  
+    // const image = await signerContract.read.getDataUri([tokenId])
+
+
+
+  return new ImageResponse(
+    (
+      <Card>
+        <div
+          style={{
+            width:"100%",
+            marginTop: "40px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+            { uri ? <img width="65%" src={uri} alt="image"></img> : <p>oops, looks like an error occured.</p>}
+
+        </div>
+      </Card>
+    ),
+    {
+      width: 1000,
+      height: 1000,
+    }
+  );
+}
