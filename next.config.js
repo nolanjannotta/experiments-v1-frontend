c3103f6b0abb37d99d501d8b514968f27babdee6
Nolan Jannotta <anjannotta@verizon.net>
2024-05-16 21:32:32 -0700

    5/16

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+    webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
-        config.module.rules.push({
-          test: /\.node$/,
-          use: [
-            {
-              loader: "nextjs-node-loader",
-            },
-          ],
-        });
+        
         return config;
       }
 }
