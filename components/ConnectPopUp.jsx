b0fc8983bc9b8ffeb7e5f1db2941f0be1b9db073
Nolan Jannotta <anjannotta@verizon.net>
2024-06-11 10:32:30 -0700

    6/11/24

--- components/ConnectPopUp.jsx
+++ components/ConnectPopUp.jsx
@@ -49,8 +49,8 @@ export default function ConnectPopUp({ show, setShow, account}) {
                     
                     </p>
 
-                {account.chainId !== baseSepolia.id && <button style={button} onClick={() =>switchChain({chainId: 84532})}>switch chains</button>}
-                    
+                {account.chainId !== baseSepolia.id && <><button style={button} onClick={() =>switchChain({chainId: 84532})}>switch chains</button><br/></>}
+                
                 {account.chainId === baseSepolia.id && balance.data?.value && <p style={{padding:"0"}}>balance: {formatEther(balance.data?.value)} eth</p>}
                  
 
