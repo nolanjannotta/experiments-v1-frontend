d5b34ee46b2155cb6aea256bf061701bcaf5ff38
Nolan Jannotta <anjannotta@verizon.net>
2024-06-28 20:44:46 -0700

    mainnet

--- app/frames/images/about/route.js
+++ app/frames/images/about/route.js
@@ -2,47 +2,47 @@ import { ImageResponse } from "next/og";
 import Card from "@/components/Card";
 
 const pageContent = [
-  <p>
+  `
     Experiments V1 is a project where I learn, explore, and
-    <i>experiment</i> with 100% onchain generative art and onchain mechanics.
+    experiment with 100% onchain generative art and onchain mechanics.
     This project is inspired by platforms like ArtBlocks and others. One
     important difference however is that here, there will never be any offchain
     code, p5.js, javascript, ipfs, or URLs to servers. Raw SVG code is generated
     and returned 100% onchain forever. This is a 'platform' for sharing and
     minting generative onchain art/ideas. It's permissioned, but open to anyone!
     click here for more information. The goal of this project is to learn a lot,
-    experiment with onchain art and mechanics, demonstrate <i>some</i>{" "}
+    experiment with onchain art and mechanics, demonstrate some
     possibilities of onchain SVGs, encourage other artists to get involved,
     develop my personal style, and emerge with a set of tools and techniques to
     work with SVG in solidity to create onchain art. Then take what I've learned
     and start thinking about a v2.
-  </p>,
+  `,
 
-  <p>
-    These 'experiments' (also'editions') are managed by a single ERC721 smart
+  `
+    These 'experiments' (also 'editions') are managed by a single ERC721 smart
     contract on the BaseL2 blockchain. Each individual edition is linked to its
     own immutable Art Generator contract. This contract is in charge of
     generating the raw SVG code and any attributes that this experiment may or
     may not have.
-  </p>,
+  `,
 
-  <p>
+  `
     Each token has 32 bits of storage that is stored in the ERC721 smart
     contract. This can be number, string, bytes, etc. When it comes to
     generative art, it is usually a 'random' number. This number can be derived
     from block data like timestamp, block hash, minters address, etc. The seed
     can also be any arbitrary data that can fit into 32 bytes, and can be
     packed, unpacked and modified by the art generator contract.
-  </p>,
+  `,
 
-  <p>
+  `
     This project is my submission to Base's Onchain Summer Buildathon 2024. To
     get it started, I've created several editions that will be deployed and
     mintable as soon as this project is ready. I will continue to create and add
     new editions over time to develop my personal style and to show the
     possibilities of onchain SVG art and hopefully inspire others to get
     involved.
-  </p>,
+  `,
 ];
 
 export async function GET(request) {
